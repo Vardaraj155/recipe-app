f4ce2cba690d2a1e8036a4d584bbeca5d67d6800
vardaraj155 <vardagitar11@gmail.com>
2021-07-14 18:56:34 +0530

    2 recent changes

--- simple.py
+++ simple.py
@@ -18,3 +18,19 @@ debut = sample(other)
 # debut
 other("mn")
 
+class Person:
+    "This is a person class"
+    age = 10
+
+    def greet(self):
+        print('Hello')
+
+
+# Output: 10
+print(Person.age)
+
+# Output: <function Person.greet>
+print(Person.greet)
+
+# Output: "This is a person class"
+print(Person.__doc__)
