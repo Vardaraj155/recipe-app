983eece622375d0dddbb32ed175c0937674a4571
vardaraj155 <vardagitar11@gmail.com>
2021-07-14 18:47:27 +0530

    2 files in example and simple changed

--- example.py
+++ example.py
@@ -32,3 +32,11 @@ def swapList(newList):
 # Driver code
 newList = [12, 35, 9, 56, 24]
 print(swapList(newList))
+
+# Collect input from the user
+kilometers = float(input('How many kilometers?: '))
+# conversion factor
+conv_fac = 0.621371
+# calculate miles
+miles = kilometers * conv_fac
+print('%0.3f kilometers is equal to %0.3f miles' % (kilometers, miles))
